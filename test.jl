bef80f218729fea0058799eae15adc17f1d74b69
Dong-Bowei <93163911+Dong-Bowei@users.noreply.github.com>
2022-08-30 10:11:14 -0500

    Update test.jl

--- test.jl
+++ test.jl
@@ -1,4 +1,8 @@
 #julia
-a=push!([1,2,3],4,5,6)
-vec(a)
-a'
+#test 
+#a=push!([1,2,3],4,5,6)
+#a
+#PS-1, 3(f).
+wage=nlsw88[:,[:industry,:occupation,:wage]]
+industry = missing, occupation = missing
+combine(grouper, valuecols(grouper) .=> mean)
